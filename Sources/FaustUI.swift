f5777a9cb2c881e9e7e8d025f2c65551fa51dc2b
Alex <anadjarov@gmail.com>
2025-03-24 17:08:20 +0100

    Update FaustUI.swift
    more classes for UI JSON handling

--- Sources/FaustUI.swift
+++ Sources/FaustUI.swift
@@ -3,11 +3,32 @@
 
 import Foundation
 
+public enum FaustUIStyle: String, Codable {
+    case knob, led, numerical
+}
+
+public enum FaustUIDerivedType {
+    case hdbbargraph, vdbbargraph, knob, menu, radio, led, numerical
+}
+
+public enum FaustUIScale {
+    case linear, log, exp
+}
+
 public enum FaustUIType: String, Codable {
     case vslider, hslider, nentry
     case checkbox, button
     case vgroup, hgroup, tgroup
     case hbargraph, vbargraph
+}
+
+public struct FaustUIMeta: Codable {
+    public let style: FaustUIStyle?
+    public let unit: String?
+}
+
+public struct FaustUIJSON: Codable {
+    public let ui: [FaustUI]?
 }
 
 public struct FaustUI: Codable, Identifiable {
@@ -18,6 +39,7 @@ public struct FaustUI: Codable, Identifiable {
     public let varname: String?
     public let shortname: String?
     public let address: String?
+    public let meta: [FaustUIMeta]?
     public let items: [FaustUI]?
 
     public let initValue: Double?
@@ -25,12 +47,20 @@ public struct FaustUI: Codable, Identifiable {
     public let max: Double?
     public let step: Double?
 
+    // private: derived from JSON
+    private var derivedType: FaustUIDerivedType? = nil
+    private var units: String? = nil
+    private var tooltip: String? = nil
+    private var scale: FaustUIScale = .linear
+    private var hidden: Bool = true
+    
     public init(
         type: FaustUIType,
         label: String,
         varname: String? = nil,
         shortname: String? = nil,
         address: String? = nil,
+        meta: [FaustUIMeta]? = nil,
         items: [FaustUI]? = nil,
         initValue: Double? = nil,
         min: Double? = nil,
@@ -42,14 +72,17 @@ public struct FaustUI: Codable, Identifiable {
         self.varname = varname
         self.shortname = shortname
         self.address = address
+        self.meta = meta
         self.items = items
         self.initValue = initValue
         self.min = min
         self.max = max
         self.step = step
+        
+        // process derived values here:
     }
 
     enum CodingKeys: String, CodingKey {
-        case type, label, varname, shortname, address, items, initValue, min, max, step
+        case type, label, varname, shortname, address, meta, items, initValue, min, max, step
     }
 }
